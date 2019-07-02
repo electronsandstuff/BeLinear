b665f9fe59e3bc3a07b48cad78288592bc438a3a
Christopher M. Pierce <cpierce963@gmail.com>
2019-07-01 22:41:06 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# BeLinear
+This library contains methods to work with linear beam optics.
---
+++ belinear/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+################################################################################
+# File: __init__.py
+# Author: Christopher M. Pierce (cmp285@cornell.edu)
+################################################################################
+
+# Import everything from the eadygdf module
+from . import belinear
+from .belinear import *
