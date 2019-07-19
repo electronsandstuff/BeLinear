1d5a2ce4fb2e2276878571575ef15e083d2ddb70
Chris Pierce <cpierce963@gmail.com>
2019-07-18 20:39:21 -0400

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='BeLinear',
+      version='1.0',
+      description='Numerical methods for computing linear transport matrices in beam optics.',
+      author='Christopher M. Pierce',
+      author_email='chris@chris-pierce.com',
+      packages=['belinear'],
+     )
